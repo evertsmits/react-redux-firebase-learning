8b1005197a1a129e9624bb7a7fb615fb3ed99b1c
Evert Smits <31696159+evertsmits@users.noreply.github.com>
2018-09-04 11:57:12 +0200

    update

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase'
 
 //CONFIG IS MISSING OFCOURSE ;) FOR SAFETY REASONS
 
-// Deze regel fixed een nieuwe table als het ware in de firebase database genaamd nots
+// Deze regel fixed een nieuwe table als het ware in de firebase database genaamd notes
 export const database = firebase.database().ref('/notes')
 export const auth = firebase.auth()
 export const googleProvider = new firebase.auth.GoogleAuthProvider()
